04978127a3b7ad4311ba222bf893c655a5675a4c
sathamhussain1993 <134479270+sathamhussain1993@users.noreply.github.com>
2023-05-24 13:01:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,32 @@
-// Make a GET request to retrieve country data from the API
-fetch('https://restcountries.com/v3.1/all')
-  .then(response => response.json())
-  .then(data => {
-    // Filter countries from Asia
-    const asiaCountries = data.filter(country => country.region === 'Asia');
+// Step 1: Fetch the data from the API
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((data) => {
+    // Step 2: Get all the countries from the Asia continent/region using the Filter function
+    const asiaCountries = data.filter((country) => country.region === 'Asia');
 
-    console.log('Countries in Asia:');
-    asiaCountries.forEach(country => {
-      console.log('Name:', country.name.common);
-      console.log('Capital:', country.capital);
-      console.log('Flag:', country.flags.svg);
-      console.log('--------------------------');
+    console.log("Countries in Asia:", asiaCountries);
+
+    // Step 3: Get all the countries with a population of less than 2 lakhs using the Filter function
+    const populationLessThan2Lakhs = data.filter((country) => country.population < 200000);
+
+    console.log("Countries with population less than 2 lakhs:", populationLessThan2Lakhs);
+
+    // Step 4: Print the following details name, capital, flag using forEach function
+    data.forEach((country) => {
+      console.log("Name:", country.name.common);
+      console.log("Capital:", country.capital);
+      console.log("Flag:", country.flags.svg);
     });
 
-    // Filter countries with a population less than 2 lakhs
-    const populationLessThan2Lakhs = data.filter(country => {
-      const population = country.population;
-      return population && population < 200000;
-    });
+    // Step 5: Print the total population of countries using reduce function
+    const totalPopulation = data.reduce((acc, country) => acc + country.population, 0);
 
-    console.log('Countries with population less than 2 lakhs:');
-    populationLessThan2Lakhs.forEach(country => {
-      console.log('Name:', country.name.common);
-      console.log('Capital:', country.capital);
-      console.log('Flag:', country.flags.svg);
-      console.log('--------------------------');
-    });
+    console.log("Total population of countries:", totalPopulation);
 
-    // Calculate the total population of countries
-    const totalPopulation = data.reduce((acc, country) => {
-      const population = country.population;
-      return acc + (population ? population : 0);
-    }, 0);
+    // Step 6: Print the country which uses US Dollars as currency
+    const countryUsingUSD = data.find((country) => country.currencies && country.currencies.USD);
 
-    console.log('Total population of countries:', totalPopulation);
-
-    // Filter the country that uses US Dollars as currency
-    const usDollarCountries = data.filter(country => {
-      const currencies = country.currencies;
-      return currencies && currencies.USD;
-    });
-
-    console.log('Countries that use US Dollars as currency:');
-    usDollarCountries.forEach(country => {
-      console.log('Name:', country.name.common);
-      console.log('Capital:', country.capital);
-      console.log('Flag:', country.flags.svg);
-      console.log('--------------------------');
-    });
+    console.log("Country using US Dollars as currency:", countryUsingUSD);
   })
-  .catch(error => console.log('Error:', error));
+  .catch((error) => console.log("Error fetching data:", error));
